fb675326c0fe2681c953a4b88f43457709a529bb
Paramvir Singh <paramvir12121@gmail.com>
2023-03-20 01:59:13 +0000

    day 4 rock-paper-scissor

---
+++ Day1-10/day4_Rock_paper_Scisssor.py
@@ -0,0 +1,37 @@
+import random
+
+player_choice = int(input(
+    "Make your move, 0 for rock, 1 for paper, 2 for scissiors: "))
+computer_choice = random.randint(0, 2)
+rpc_array = ["""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""", """
+     _______
+---'    ____)________
+           __________)
+          ___________)
+         ___________)
+---.______________)
+""", """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""]
+
+print(f"You choose {rpc_array[player_choice]}")
+print(f"Computer choose {rpc_array[computer_choice]}")
+
+if player_choice == computer_choice:
+    print("Its a draw")
+elif (player_choice == 1 and computer_choice == 0) or (player_choice == 2 and computer_choice == 1) or (player_choice == 0 and computer_choice == 2):
+    print("You win")
+if (player_choice == 0 and computer_choice == 1) or (player_choice == 1 and computer_choice == 2) or (player_choice == 2 and computer_choice == 0):
+    print("You lose")
