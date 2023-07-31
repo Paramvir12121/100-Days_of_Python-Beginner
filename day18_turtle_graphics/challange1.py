9fdb65492a5fb37c5eb56b759618fe1d84283724
paramvir12121 <paramvir12121@gmail.com>
2023-07-31 01:20:41 -0400

    day 18 challange 1

---
+++ day18_turtle_graphics/challange1.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+
+
+#challange 1
+#draw a square
+
+for side in range(0,4):
+    turtle.forward(100)
+    turtle.right(90)
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
+
---
+++ day18_turtle_graphics/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+
+timmy_the_turtle = Turtle()
+
+screen = Screen()
+
+
+
+
+
+
+
+screen.exitonclick()
