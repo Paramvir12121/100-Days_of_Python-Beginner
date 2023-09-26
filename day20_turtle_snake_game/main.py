6a442f1a120fbc3a4a77872f4effc6afb8bbfcc3
paramvir12121 <paramvir12121@gmail.com>
2023-09-26 00:07:59 -0400

    snake movement complete- opposite movement bug fix

--- day20_turtle_snake_game/main.py
+++ day20_turtle_snake_game/main.py
@@ -1,3 +1,7 @@
+from turtle import Turtle, Screen
+from snake import Snake
+import time
+
 # 7 steps to creating a snake game
 # #1 Creating a Snake body
 # #2 Moving Said body
@@ -7,44 +11,28 @@
 # #6 Detect collision with wall
 # #7 Adding a delay to the snake
 
-from turtle import Turtle, Screen
-import time
-
 screen = Screen()
 screen.setup(width=700, height=700)
 screen.tracer(0)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
-#1 Snake Body
-segments = []
-starting_pos = [(0,0),(-20,0),(-40,0)]
 
-for i in range(3):
-    new_snake_square = Turtle(shape="square")
-    new_snake_square.color("white")
-    new_snake_square.penup()
-    new_snake_square.goto(starting_pos[i])
-    segments.append(new_snake_square)
+snake = Snake()
 
-
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right,"Right")
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(.1)
-    for seg_num in range(len(segments) -1, 0,-1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x,new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
+    snake.move()
     
-
-
-
-
 
 
 screen.exitonclick()
