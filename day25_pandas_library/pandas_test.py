6d62326029d1271cdb83160be143ddcac7e2d9fe
Paramvir Singh <paramvir12121@gmail.com>
2023-10-10 02:53:55 +0000

    day 25 changes

--- day25_pandas_library/pandas_test.py
+++ day25_pandas_library/pandas_test.py
@@ -1,5 +1,15 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
+data_dict = data.to_dict()
 
-print(data["temp"])
+tempratures = data["temp"].to_list()
+
+total_temp = 0
+
+for temp in tempratures:
+    total_temp += temp
+
+avg_temp = total_temp/(len(tempratures))
+
+print(avg_temp)
