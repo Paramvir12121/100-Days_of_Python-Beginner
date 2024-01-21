1ad6ae93ea9345354d1db86b8ce83a224f4c7182
paramvir12121 <paramvir12121@gmail.com>
2024-01-21 09:01:34 -0500

    squrrel color data

--- day25_pandas_library/squirel.py
+++ day25_pandas_library/squirel.py
@@ -4,4 +4,18 @@ data = pandas.read_csv("squirrel_data.csv")
 
 color_list = data["Primary Fur Color"].unique()
 
-print(color_list)
+gray_coat_squirel = len(data[data["Primary Fur Color"]== "Gray"])
+cinnamon_coat_squirel = len(data[data["Primary Fur Color"]== "Cinnamon"])
+black_coat_squirel = len(data[data["Primary Fur Color"]== "Black"])
+
+print(gray_coat_squirel,"\n",cinnamon_coat_squirel,"\n",black_coat_squirel)
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon","Black"],
+    "Squirrel Count": [gray_coat_squirel,cinnamon_coat_squirel,black_coat_squirel]
+}
+
+df = pandas.DataFrame(data_dict)
+
+print(df)
+
+df.to_csv("squirrel_by_color")
