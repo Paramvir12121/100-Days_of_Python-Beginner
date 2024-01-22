f0f56c57ff284e0e0cd7d38c14b2856fd3cbe9a2
paramvir12121 <paramvir12121@gmail.com>
2024-01-22 10:51:45 -0500

    added loop

--- day25_pandas_library/US_States-Game/main.py
+++ day25_pandas_library/US_States-Game/main.py
@@ -1,6 +1,8 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
+pen = turtle.Turtle()
 screen.title("US States Game")
 image = "blank_states_img.gif"
 
@@ -11,15 +13,20 @@ turtle.shape(image)
 # Code to get coordintes on the screen where mouse clickes
 # def get_mouse_click_coor(x,y):
 #     print(x,y)
-
 # turtle.onscreenclick(get_mouse_click_coor)
-
 # turtle.mainloop()
 
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's Another State")
+end_toggle = False
 
-print(answer_state)
+while end_toggle == False:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's Another State")
+    print(answer_state)
+    if answer_state is None:
+        end_toggle = True
+
+    
+
 
 
 
