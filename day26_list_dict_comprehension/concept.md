05ccdf683105a7ec8dac6615f02dea46c4a3fbd2
Paramvir Singh <paramvir12121@gmail.com>
2024-01-24 11:07:09 +0000

    example file done

--- day26_list_dict_comprehension/concept.md
+++ day26_list_dict_comprehension/concept.md
@@ -6,3 +6,5 @@ It is a way to create a list from previous list.
 - It is unique to python and makes code a lot shorther.
 
 - Not limited to list, can also be a string, dict, etc
+
+- We can also set conditions to what could be added to the list eg. : new_list = [new_item for item in list if test]
--- day26_list_dict_comprehension/example.py
+++ day26_list_dict_comprehension/example.py
@@ -23,3 +23,9 @@ print(name_list)
 range_list = [n*2 for n in range(1,5)]
 print(range_list)
 
+## We can add conditional statemetns
+names = ["Harry", "Alex","James","Beth"]
+short_names = [name for name in names if len(name)<5]
+capitol_long_names = [name.upper() for name in names if len(name)> 4]
+print(short_names)
+print(capitol_long_names)
