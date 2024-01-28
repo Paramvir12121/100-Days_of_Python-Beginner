1eba6eb7469b11aa2f465f0b9c2f2d901c46ad58
paramvir12121 <paramvir12121@gmail.com>
2024-01-28 13:25:36 -0500

    day 25 -added logic to main

--- day25_pandas_library/US_States-Game/main.py
+++ day25_pandas_library/US_States-Game/main.py
@@ -21,17 +21,22 @@ data = pandas.read_csv("50_states.csv")
 states = data["state"].to_list()
 
 print(states)
+state_found = []
 
 end_toggle = False
 
 while end_toggle == False:
     answer_state = screen.textinput(title="Guess the State", prompt="What's Another State")
     print(answer_state)
-    if answer_state is None:
+    if answer_state == 'end':
         end_toggle = True
-    # for state in data["state"]:
-    #     if answer_state == data
-
+    elif answer_state in data["state"].values:
+        state_found.append(answer_state)
+        x_value = data[data['state'] == answer_state]['x'].iloc[0]
+        y_value = data[data['state'] == answer_state]['y'].iloc[0]
+        print(f"State: {answer_state}, X: {x_value}, Y: {y_value}")
+    else:
+        print("wrong input")
     
 
 
