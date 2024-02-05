6e30d43483d85713ce0d036e4e45345e0fe267cc
Paramvir Singh <paramvir12121@gmail.com>
2024-02-05 06:47:38 +0000

    more efficient code

--- day26_list_dict_comprehension/nato_alphabet_project.py
+++ day26_list_dict_comprehension/nato_alphabet_project.py
@@ -9,21 +9,13 @@ df = pandas.read_csv("alphabet_conversion.csv")
 dict = {row.letter:row.code for (index,row) in df.iterrows()}
 
 print(dict)
-
-
-def find_letter(letter):
-    for (key,val) in dict.items():
-        if key == letter:
-            print(val)
-            return val
     
-
 
 input_word = input("Enter a word: ").upper()
 
 input_word = list(input_word)
 
-nato_code = [find_letter(letter) for letter in input_word]
+nato_code = [dict[letter] for letter in input_word]
 
 print(nato_code)
 
