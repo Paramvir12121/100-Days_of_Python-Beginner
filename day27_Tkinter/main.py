dad1e11c9a62ae729f4ba57324de9ac6d60a043a
paramvir12121 <paramvir12121@gmail.com>
2024-02-08 14:26:59 -0500

    day 27 minor changes

--- day27_Tkinter/main.py
+++ day27_Tkinter/main.py
@@ -4,6 +4,8 @@ window = tkinter.Tk()
 window.title("My first GUI")
 window.minsize(width=500,height=300)
 
+my_label = tkinter.Label(text="This is a label", font=("Arial",18,"bold"))
+my_label.pack()
 
 
 
