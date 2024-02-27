d8cab8fc1136b81328054b00b87a5fe0f6e5ad37
paramvir12121 <paramvir12121@gmail.com>
2024-02-27 11:41:59 -0500

    started on timer

--- day28_tkinter_pomodoro/main.py
+++ day28_tkinter_pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import time
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,27 +11,51 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-window = Tk()
-window.title("Pomodoro")
-window.config(padx=100,pady=50,bg=YELLOW)
-
-canvas = Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100,112,image=tomato_img)
-canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-canvas.pack()
 
 
 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    print(count)
+    if count > 0:
+        window.after(1000,count_down,count-1)
+    
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100,pady=50,bg=YELLOW)
 
+window.after(1000,count_down,5)
+
+
+
+
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100,112,image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+# ---------------------------- BUTTONS ------------------------------- #
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
 
 
 
