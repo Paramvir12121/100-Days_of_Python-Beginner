610b129bc9d5e9cd89240206340dddff92a72692
Paramvir Singh <paramvir12121@gmail.com>
2024-02-29 10:58:27 +0000

    post working

---
+++ day37_habit_tracker_api_requests/post_work.py
@@ -0,0 +1,31 @@
+import requests
+from dotenv import load_dotenv
+import os
+from datetime import date
+
+
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+load_dotenv()
+token = os.getenv('PIXELA_TOKEN')
+
+username = "param12121"
+headers = {
+    "X-USER-TOKEN": token
+}
+graph2_endpoint = f"{pixela_endpoint}/{username}/graphs/graph2"
+
+today = date.today()
+today = today.strftime("%Y%m%d")
+print(today)
+
+pixal_data = {
+    "date": today,
+    "quantity": "1",
+   
+
+}
+
+response = requests.post(url=graph2_endpoint,json=pixal_data,headers=headers)
+print(response.json())
