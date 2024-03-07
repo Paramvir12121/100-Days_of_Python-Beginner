77e7f96898ee8fb4e64ece5e1b727a95975c8b37
Paramvir Singh <paramvir12121@gmail.com>
2024-03-07 09:47:24 +0000

    started day 54

---
+++ day54_flask_beginner/concept.md
@@ -0,0 +1,5 @@
+## Concpets
+- Name of the file should not conflict with name of the framework or library.
+
+
+# __name__ and 
---
+++ day54_flask_beginner/hello.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+print(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+# to run whithout using cmd >flask --app hello run 
+# Allows to just use python hello.py
+if __name__ == "__main__":
+    app.run()
