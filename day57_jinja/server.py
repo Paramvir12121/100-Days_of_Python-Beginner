f2c1102b2ad0c19f8a79992481502c97d09c364f
Paramvir Singh <paramvir12121@gmail.com>
2024-03-11 08:45:08 +0000

    added api use in flask

--- day57_jinja/server.py
+++ day57_jinja/server.py
@@ -1,12 +1,25 @@
 from flask import Flask,render_template
-import random
+import random, time, requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     random_number = random.randint(0,9)
-    return render_template("index.html",num=random_number)
+    current_time = time.localtime()
+    current_year = current_time.tm_year 
+    return render_template("index.html",num=random_number, year=current_year)
+
+
+@app.route("/guess/<user_name>")
+def guess(user_name):
+    params = {
+        "name": user_name
+    }
+    response = requests.get("https://api.genderize.io",params=params)
+    genderize = response.json()
+    gender = genderize.gender
+    return render_template('guess.html',gender=gender)
 
 
 # to run whithout using cmd >flask --app <filename> run 
